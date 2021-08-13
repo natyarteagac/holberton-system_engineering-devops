d690fc8dbff75957057cdabdc50a4fa97c7e5f3a
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-13 19:38:33 +0000

    Starting with the project

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This file is going to create a new file in /tmp
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
