31b6e2959b52c70be43469abafb195cf0e164c58
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-18 14:19:27 -0500

    Killing process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Manifiest that kill a process named "killmenow"
 
 exec { 'killmenow':
-  command  => 'pkill -i',
+  command  => 'pkill -f killmenow',
   provider => shell,
-  onlyif   => 'killmenow',
 }
