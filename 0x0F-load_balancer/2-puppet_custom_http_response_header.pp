e005a78fdda80ef118bb50bf9eb31fcbf82042b2
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-23 21:29:16 -0500

    Fixing changes

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,12 @@
 # Creating a HTTP header X-Served-By
 
 exec {
-  cwd      => /etc/nginx/nginx.conf,
+  command  => 'sudo apt-get -y update',
+  command  => 'sudo apt-get -y upgrade',
+  command  => 'sudo apt-get -y install nginx',
+  command  => 'sudo service nginx start',
   command  => 'sudo sed -i 11i \\\t add_header X-Served-By '$HOSTNAME' always;\n',
+  cwd      => /etc/nginx/nginx.conf,
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
