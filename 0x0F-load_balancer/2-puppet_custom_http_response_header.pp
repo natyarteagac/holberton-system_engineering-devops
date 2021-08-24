495d48c47e037c5b7e1a0ba908bbd4a84c942f05
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-24 11:51:28 -0500

    Trying with check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,10 +6,13 @@ exec { 'executing in nginx':
                sudo apt-get -y install nginx;
                sudo service nginx start;
                sudo chmod 777 /var/www/html/index.nginx-debian.html;
-               echo 'Holberton School' | sudo tee /usr/share/nginx/html/custom_404.html;
-               /etc/nginx/sites-enabled/default/sudo sed -i '20i \\\terror_page 404\
-            /custom_404.html; \n\tlocation = /custom_404.html {\n\t root /usr/share/nginx/html;\n\tinternal;\n\t}';
-               /etc/nginx/nginx.conf/sudo sed -i 11i \\\t add_header X-Served-By $servername always;\n;
+               echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;
+               /etc/nginx/sites-enabled/default/sudo sed -i /server_name _;/ a \\trewrite\
+             ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+               echo "C'est pas a page" | sudo tee /usr/share/nginx/html/custom_404.html;
+               /etc/nginx/sites-enabled/default/sudo sed -i 20i \\\terror_page 404 /custom_404.html;\n\tlocation =\
+             /custom_404.html {\n\t root /usr/share/nginx/html;\n\tinternal;\n\t};
+               sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;
                sudo service nginx restart',
   provider => shell,
 }
