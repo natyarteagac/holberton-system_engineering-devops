3c1d739e38a6935319497ddba8768bc35972977b
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-24 11:54:48 -0500

    I don't know what to do

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,9 +9,6 @@ exec { 'executing in nginx':
                echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;
                /etc/nginx/sites-enabled/default/sudo sed -i /server_name _;/ a \\trewrite\
              ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-               echo "C'est pas a page" | sudo tee /usr/share/nginx/html/custom_404.html;
-               /etc/nginx/sites-enabled/default/sudo sed -i 20i \\\terror_page 404 /custom_404.html;\n\tlocation =\
-             /custom_404.html {\n\t root /usr/share/nginx/html;\n\tinternal;\n\t};
                sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;
                sudo service nginx restart',
   provider => shell,
