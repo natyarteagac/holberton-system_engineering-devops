8556e6c8df810e13bed743d114c7110f101d67e3
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-23 21:40:11 -0500

    Fixing checks!

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,8 +5,7 @@ exec {
   command  => 'sudo apt-get -y upgrade',
   command  => 'sudo apt-get -y install nginx',
   command  => 'sudo service nginx start',
-  command  => 'sudo sed -i 11i \\\t add_header X-Served-By '$HOSTNAME' always;\n',
-  cwd      => /etc/nginx/nginx.conf,
+  command  => '/etc/nginx/nginx.conf/sudo sed -i 11i \\\t add_header X-Served-By 'hostname' always;\n',
   command  => 'sudo service nginx restart',
   provider => shell,
 }
