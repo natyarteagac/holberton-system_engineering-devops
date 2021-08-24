e9e51b0a0a4c7e97e365544d7f371bdf09435380
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-23 21:41:59 -0500

    Giving a name

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,11 @@
 # Creating a HTTP header X-Served-By
 
-exec {
+exec { 'executing in nginx':
   command  => 'sudo apt-get -y update',
   command  => 'sudo apt-get -y upgrade',
   command  => 'sudo apt-get -y install nginx',
   command  => 'sudo service nginx start',
-  command  => '/etc/nginx/nginx.conf/sudo sed -i 11i \\\t add_header X-Served-By 'hostname' always;\n',
+  command  => '/etc/nginx/nginx.conf/sudo sed -i 11i \\\t add_header X-Served-By $servername always;\n',
   command  => 'sudo service nginx restart',
   provider => shell,
 }
