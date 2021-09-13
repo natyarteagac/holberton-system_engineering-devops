c6b214818e998268049b08a384fc77d04d4957fe
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-13 17:32:40 -0500

    Starting with project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,5 +5,16 @@ from sys import argv
 import requests
 import json
 
-response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-print(response.json())
+response = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])).json()
+name_of_employee = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+counter = 0
+for index in response:
+    if index.get('completed') is True:
+        counter += 1
+print("Employee {} is done with tasks ({}/{}):".format(
+    name_of_employee.get('name'), counter, len(response)))
+for index in response:
+    if index.get('completed') is True:
+        print("{}".format(index.get('title')))
