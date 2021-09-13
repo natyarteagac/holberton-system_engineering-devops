efe07a4c27940894de6758f8d18f077b0b78cb1b
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-13 18:23:47 -0500

    Trying

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Export to CSV """
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".
+        format(argv[1])).json()
+    name_of_employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+
+    with open("{}.csv".format(argv[1]), "w") as file_csv:
+        f = csv.writer(file_csv)
+        f.writerow("response", "name_of_employee")
+        for index in response:
+            f.writerows("{}, {}, {}".format(index.get('Id'), index.get(
+                'completed'), index.get('title')))
