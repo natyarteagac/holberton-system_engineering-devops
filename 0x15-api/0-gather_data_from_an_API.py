98438031fabb4583b540b5c55e278200df55df18
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-13 17:43:49 -0500

    With if __name__

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,18 @@
 import requests
 from sys import argv
 
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}/todos".
-    format(argv[1])).json()
-name_of_employee = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
-counter = 0
-for index in response:
-    if index.get('completed') is True:
-        counter += 1
-print("Employee {} is done with tasks({}/{}): ".format(
-    name_of_employee.get('name'), counter, len(response)))
-for index in response:
-    if index.get('completed') is True:
-        print("\t {}".format(index.get('title')))
+if __name__ == '__main__':
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".
+        format(argv[1])).json()
+    name_of_employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+    counter = 0
+    for index in response:
+        if index.get('completed') is True:
+            counter += 1
+    print("Employee {} is done with tasks({}/{}): ".format(
+        name_of_employee.get('name'), counter, len(response)))
+    for index in response:
+        if index.get('completed') is True:
+            print("\t {}".format(index.get('title')))
