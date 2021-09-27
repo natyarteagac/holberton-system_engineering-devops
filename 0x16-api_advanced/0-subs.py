f37ef7aaa48d032f49f20a0878ae537cab092de0
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-27 17:39:46 -0500

    Starting with project

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and
+returns the number of subscribers"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    headers = {
+        "User-Agent": "Mozilla/5.0:APIrequest1.0 (by u/natyarteagac)"
+    }
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.
+        format(subreddit), headers=headers, allow_redirects=False).json()
+    number_of_suscribers = r.get('data').get('subscribers')
+    if not subreddit:
+        return 0
+    else:
+        return number_of_suscribers
